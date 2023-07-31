8fbab5c9b959070c0ee22914c629551c0b65a39e
Xiaokang Li <xiaokangli616@gmail.com>
2023-07-31 18:59:35 +0800

    set items to circle shape.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,68 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import TreeChart from 'vue-tree-chart-3';
+
+enum Colors {
+  GREEN = 'https://upload.wikimedia.org/wikipedia/commons/c/c5/Pure_Green.svg',
+  RED = 'https://upload.wikimedia.org/wikipedia/commons/6/62/Solid_red.svg',
+};
+
+let treeData =   {
+    name: 'root',
+    image_url: Colors.RED,
+    class: ["rootNode"],
+    children: [
+      {
+        name: 'children1',
+        image_url: Colors.GREEN
+      },
+      {
+        name: 'children2',
+        image_url: Colors.GREEN,
+        children: [
+          {
+            name: 'grandchild',
+            image_url: Colors.GREEN
+          },
+          {
+            name: 'grandchild2',
+            image_url: Colors.RED
+          },
+          {
+            name: 'grandchild3',
+            image_url: Colors.GREEN
+          }
+        ]
+      },
+      {
+        name: 'children3',
+        image_url: Colors.RED
+      }
+    ]
+  };
+
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <body>
+    <TreeChart :json="treeData" />
+  </body>
 
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
-<style scoped>
-nav {
-  width: 100%;
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#app .avat{border-radius: 2em;border-width:2px;}
+#app .name{font-weight:700;}
+#app .rootNode .name{
+  color: red;
 }
 </style>
