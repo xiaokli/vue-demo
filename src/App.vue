f1006ed0479b32569b474f5e573f52eb365abdbd
Xiaokang Li <xiaokangli616@gmail.com>
2023-07-31 13:36:36 +0800

    create VUE project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+function test() {
+  alert("this is from")
+
+  return function() {
+    alert("inside")
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1> this Home page</h1>
+  </div>
+</template>
